87ae68ed72890d60f7f5f21ddb2e04c59f984ea8
Jondahl Rhenius <jondahlrh@gmail.com>
2022-06-24 19:14:46 +0200

    fixxed switch, func for coach//it/kummer activated

--- index.js
+++ index.js
@@ -23,42 +23,91 @@ TeamSpeak.connect({
     const support = transform(await teamspeak.getChannelById("24"));
     const supportRang = transform(await teamspeak.getChannelById("25"));
     const supportAppl = transform(await teamspeak.getChannelById("27"));
-    // const supportIT = transform(await teamspeak.getChannelById("22621"));
-    // const supportCoach = transform(await teamspeak.getChannelById("61154"));
-    // const supportKummer = transform(await teamspeak.getChannelById("68837"));
+    const supportIT = transform(await teamspeak.getChannelById("22621"));
+    const supportCoach = transform(await teamspeak.getChannelById("61154"));
+    const supportKummer = transform(await teamspeak.getChannelById("68837"));
 
     // function when a client changes channel
     const clientMovedHandler = async (eventRAW) => {
       const event = transform(eventRAW);
-
-      // check switched channel
+      if (event.client.clientType === 1) return; // return if server query user
+      const clientList = await teamspeak.clientList({ clientType: 0 });
       switch (event.client.cid) {
-        // switched to support, supportRang or supportAppl
-        case support.cid || supportRang.cid || supportAppl.cid:
-          // get client list
-          const clientList = await teamspeak.clientList();
-
-          // loop client list
+        case support.cid:
+        case supportRang.cid:
+        case supportAppl.cid:
           clientList.forEach((clientRAW) => {
             const client = transform(clientRAW);
-
-            // groups who should get a message
-            if (!client.clientServergroups.includes("2552")) {
+            console.log(client.clientNickname);
+            if (
+              // !client.clientServergroups.includes("85") || // Admin
+              // !client.clientServergroups.includes("77") || // Head-Mod
+              // !client.clientServergroups.includes("10") || // Mod
+              // !client.clientServergroups.includes("96") || // Trial-Mod
+              // !client.clientServergroups.includes("260") || // Supp-Benachrichtigung
+              !client.clientServergroups.includes("2552") // GamixTestGruppe
+            ) {
               return;
-            }
-            // groups who should NOT get a message
-            if (client.clientServergroups.includes("59")) {
-              return;
-            }
-            // channel where no message should be send
-            if (client.clientChannelGroupInheritedChannelId.includes("13")) {
-              return;
-            }
-
-            // send message
+            } // groups that should get a message
+            if (client.clientServergroups.includes("59")) return; // groups that should NOT get a message
+            if (client.clientChannelGroupInheritedChannelId.includes("13")) return; // channel where you should NOT get a message
             const msgClicker = `[URL=client://${event.client.cid}/${event.client.clientUniqueIdentifier}]${event.client.clientNickname}[/URL]`;
             clientRAW.message(
-              `Der User ${msgClicker} wartet in "${event.channel.channelName}"`
+              `Der User ${msgClicker} wartet in "${event.channel.channelName}" auf einen Supporter!`
+            );
+          });
+          break;
+        case supportIT.cid:
+          clientList.forEach((clientRAW) => {
+            const client = transform(clientRAW);
+            if (
+              !client.clientServergroups.includes("328") || // IT-Berater
+              !client.clientServergroups.includes("2552") // GamixTestGruppe
+            ) {
+              return;
+            } // groups that should get a message
+            if (client.clientServergroups.includes("59")) return; // groups that should NOT get a message
+            if (client.clientChannelGroupInheritedChannelId.includes("13")) return; // channel where you should NOT get a message
+            const msgClicker = `[URL=client://${event.client.cid}/${event.client.clientUniqueIdentifier}]${event.client.clientNickname}[/URL]`;
+            clientRAW.message(
+              `Der User ${msgClicker} wartet in "${event.channel.channelName}" auf einen IT-Berater!`
+            );
+          });
+          break;
+        case supportCoach.cid:
+          clientList.forEach((clientRAW) => {
+            const client = transform(clientRAW);
+            if (
+              !client.clientServergroups.includes("95") || // Head-Coach
+              !client.clientServergroups.includes("73") || // Coach
+              !client.clientServergroups.includes("230") || // Trial-Coach
+              !client.clientServergroups.includes("2552") // GamixTestGruppe
+            ) {
+              return;
+            } // groups that should get a message
+            if (client.clientServergroups.includes("59")) return; // groups that should NOT get a message
+            if (client.clientChannelGroupInheritedChannelId.includes("13")) return; // channel where you should NOT get a message
+            const msgClicker = `[URL=client://${event.client.cid}/${event.client.clientUniqueIdentifier}]${event.client.clientNickname}[/URL]`;
+            clientRAW.message(
+              `Der User ${msgClicker} wartet in "${event.channel.channelName}" auf einen Coach!`
+            );
+          });
+          break;
+        case supportKummer.cid:
+          clientList.forEach((clientRAW) => {
+            const client = transform(clientRAW);
+            if (
+              !client.clientServergroups.includes("2484") || // Kummerkasten
+              !client.clientServergroups.includes("2485") || // Kummerkasen-Icon
+              !client.clientServergroups.includes("2552") // GamixTestGruppe
+            ) {
+              return;
+            } // groups that should get a message
+            if (client.clientServergroups.includes("59")) return; // groups that should NOT get a message
+            if (client.clientChannelGroupInheritedChannelId.includes("13")) return; // channel where you should NOT get a message
+            const msgClicker = `[URL=client://${event.client.cid}/${event.client.clientUniqueIdentifier}]${event.client.clientNickname}[/URL]`;
+            clientRAW.message(
+              `Der User ${msgClicker} wartet in "${event.channel.channelName}" und braucht Hilfe!`
             );
           });
           break;
