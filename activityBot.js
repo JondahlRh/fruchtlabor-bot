de2e419469a7fe64198d7fd3f600628c1570f10d
Jondahl Rhenius <jondahlrh@gmail.com>
2022-07-02 20:34:07 +0200

    reworked activity bot

--- activityBot.js
+++ activityBot.js
@@ -1,38 +1,12 @@
 const CREDS = require("./.creds");
 const { TeamSpeak } = require("ts3-nodejs-library");
-const { performance } = require("perf_hooks");
-const app = require("express")();
-// const fs = require("fs");
+const express = require("express");
 
-const transform = (data) => {
+const transformData = (data) => {
   return JSON.parse(JSON.stringify(data));
 };
 
 const activityList = {};
-
-const checkTalking = async (teamspeak) => {
-  const clientList = transform(await teamspeak.clientList({ clientType: 0 }));
-  for (const client of clientList) {
-    const CUID = client.clientUniqueIdentifier;
-
-    if (!Object.keys(activityList).includes(CUID)) {
-      activityList[CUID] = {
-        name: client.clientNickname,
-        active: 0,
-        online: 0,
-        lastLoop: performance.now(),
-      };
-    }
-
-    const now = performance.now();
-    if (client.clientFlagTalking) {
-      activityList[CUID].active += now - activityList[CUID].lastLoop;
-    }
-    activityList[CUID].online += now - activityList[CUID].lastLoop;
-    activityList[CUID].lastLoop = now;
-  }
-  checkTalking(teamspeak);
-};
 
 TeamSpeak.connect({
   host: CREDS.SERVER_IP,
@@ -44,26 +18,68 @@ TeamSpeak.connect({
   .then(async (teamspeak) => {
     console.log("connected");
 
-    // code below
+    // get the bot client and move it to the support spacer
+    // const self = await teamspeak.getClientByUid("Su5GYQbW1FfV4uXEaUxR7s8aeOg=");
+    // teamspeak.clientMove(self, await teamspeak.getChannelById("19"));
 
-    const self = await teamspeak.getClientByUid("Su5GYQbW1FfV4uXEaUxR7s8aeOg=");
-    teamspeak.clientMove(self, await teamspeak.getChannelById("19"));
+    // #
+    // code below
+    // #
+
+    const activityBot = async () => {
+      // get client List (online Clients)
+      const clientList = transformData(await teamspeak.clientList({ clientType: 0 }));
+
+      const now = Date.now();
+
+      for (const client of clientList) {
+        const CUID = client.clientUniqueIdentifier;
+
+        if (!activityList[CUID]) {
+          activityList[CUID] = {
+            name: client.clientNickname,
+            active: 0,
+            online: 0,
+            last: now,
+          };
+        }
+
+        if (client.clientFlagTalking) {
+          activityList[CUID].active += now - activityList[CUID].last;
+        }
+        activityList[CUID].online += now - activityList[CUID].last;
+      }
+
+      for (const CUID of Object.keys(activityList)) {
+        activityList[CUID].last = now;
+      }
+
+      setTimeout(() => {
+        activityBot();
+      }, 1000);
+    };
+
+    activityBot();
 
     // setInterval(() => {
-    //   fs.writeFile("./activityList.json", JSON.stringify(activityList), () => {});
-    // }, 1000);
+    //   console.log(activityList);
+    // }, 5000);
 
-    checkTalking(teamspeak);
-
+    // #
     // code above
+    // #
   })
-  .catch((e) => {
+  .catch((error) => {
     console.log("Catched an error!");
-    console.log(e);
+    console.log(error);
   });
 
-app.listen(8080);
+const app = express();
+
+app.listen(2999, () => {
+  console.log("Server running on port 2999");
+});
 
 app.get("/", (req, res) => {
-  res.status(200).send(activityList);
+  res.json(activityList);
 });
