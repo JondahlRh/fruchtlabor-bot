1fb630d70f9314f40965dda6c110b57692cb7b9a
Etienne M <etienneandm@gmail.com>
2022-11-18 17:10:05 +0100

    try catch added for server channel edits

--- comp/server.js
+++ comp/server.js
@@ -1,24 +1,35 @@
 const query = require("source-server-query");
 
 const online = async (teamspeak) => {
-  const serverNoAwp = await query.info("5.9.50.9", 27003);
-  const serverComMaps = await query.info("5.9.50.9", 27004);
-  const serverDuels = await query.info("5.9.50.9", 25005);
-  const serverDefault = await query.info("5.9.50.9", 27011);
+  let serverNoAwp;
+  let serverComMaps;
+  let serverDuels;
+  let serverDefault;
+
+  try {
+    serverNoAwp = await query.info("5.9.50.9", 27003);
+    serverComMaps = await query.info("5.9.50.9", 27004);
+    serverDuels = await query.info("5.9.50.9", 25005);
+    serverDefault = await query.info("5.9.50.9", 27011);
+  } catch (error) {
+    console.log({n: "Server Error", error})
+  }
 
   // console.log({ serverNoAwp, serverDefault, serverComMaps, serverDuels });
 
   teamspeak.channelEdit("131494", {
-    channelName: `● Duels (1v1) | Online: ${serverDuels.players || 0}`,
+    channelName: `● Duels (1v1) | Online: ${serverDuels?.players || 0}`,
   });
   teamspeak.channelEdit("15094", {
-    channelName: `● Retakes NoAWP | Online: ${serverNoAwp.players || 0}`,
+    channelName: `● Retakes NoAWP | Online: ${serverNoAwp?.players || 0}`,
   });
   teamspeak.channelEdit("51566", {
-    channelName: `● Retakes Standard | Online: ${serverDefault.players || 0}`,
+    channelName: `● Retakes Standard | Online: ${serverDefault?.players || 0}`,
   });
   teamspeak.channelEdit("18855", {
-    channelName: `● Retakes Communitymaps | Online: ${serverComMaps.players || 0}`,
+    channelName: `● Retakes Communitymaps | Online: ${
+      serverComMaps?.players || 0
+    }`,
   });
 };
 
