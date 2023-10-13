e70ee80db64f66650de18fc28f0ec34eab236a63
Jondahl Rhenius <jondahlrh@gmail.com>
2023-10-13 17:13:36 +0200

    added required flag

--- src/models/functions/OnlineChannel.js
+++ src/models/functions/OnlineChannel.js
@@ -6,7 +6,7 @@ import TsServergroup from "../teamspeak/TsServergroup.js";
 const { ObjectId } = mongoose.Schema.Types;
 const OnlineChannelSchema = new mongoose.Schema({
   channel: { type: ObjectId, ref: TsChannel, require: true },
-  servergroups: [{ type: ObjectId, ref: TsServergroup }],
+  servergroups: [{ type: ObjectId, ref: TsServergroup, require: true }],
   title: { type: String, require: true },
 });
 
