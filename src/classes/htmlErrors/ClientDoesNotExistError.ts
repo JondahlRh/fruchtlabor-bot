838a06f3edaf0cf1aaf0a8c412bfb4c7edf9580c
Jondahl Rhenius <jondahlrh@gmail.com>
2024-01-28 02:05:32 +0100

    Changed Error Message

--- src/classes/htmlErrors/ClientDoesNotExistError.ts
+++ src/classes/htmlErrors/ClientDoesNotExistError.ts
@@ -4,6 +4,6 @@ import KeyValueError from "../KeyValueError";
 export default class ClientDoesNotExistError extends KeyValueError {
   constructor(key: string, value: any) {
     const field = new KeyValue(key, value);
-    super("The provided client does not exist or is not online!", 400, field);
+    super("The provided client does not exist!", 400, field);
   }
 }
