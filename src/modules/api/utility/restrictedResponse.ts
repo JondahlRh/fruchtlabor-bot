bdcc12969a4ea7708b1be8ada24cecadfcb295fc
Jondahl Rhenius <jondahlrh@gmail.com>
2024-03-25 17:42:59 +0100

    changed wording

--- src/modules/api/utility/restrictedResponse.ts
+++ src/modules/api/utility/restrictedResponse.ts
@@ -2,8 +2,8 @@ import { Response } from "express";
 
 import HtmlResponse from "../../../classes/HtmlResponse";
 
-const restrictedResponse = <T>(res: Response, error: HtmlResponse<T>) => {
-  res.json(error);
+const restrictedResponse = <T>(res: Response, response: HtmlResponse<T>) => {
+  res.json(response);
 };
 
 export default restrictedResponse;
