05b21cab39331daa3c485f486861ec88c32bdf80
Jondahl Rhenius <jondahlrh@gmail.com>
2024-03-25 18:06:12 +0100

    added optional statuscode to html response

--- src/classes/HtmlResponse.ts
+++ src/classes/HtmlResponse.ts
@@ -1,9 +1,11 @@
 export default class HtmlResponse<T> {
   message: string;
+  statusCode: number;
   data: T;
 
-  constructor(message: string, data: T) {
+  constructor(message: string, data: T, statusCode: number = 200) {
     this.message = message;
+    this.statusCode = statusCode;
     this.data = data;
   }
 }
