2487bfa2aa6115122fb2dd4634ab401a60812a12
Jondahl Rhenius <jondahlrh@gmail.com>
2024-04-20 11:56:49 +0200

    added types to bot error wrapper

--- src/modules/bot/utility/asyncErrorHandler.ts
+++ src/modules/bot/utility/asyncErrorHandler.ts
@@ -1,3 +1,5 @@
+import { TeamSpeak, TeamSpeakClient } from "ts3-nodejs-library";
+
 import { saveErrorLog } from "./mongodb";
 
 const catcher = async (error: Error, functionname: string) => {
@@ -16,10 +18,9 @@ const catcher = async (error: Error, functionname: string) => {
   }
 };
 
-// eslint-disable-next-line
-export default (fn: Function) =>
-  // eslint-disable-next-line
-  (...args: any) =>
+type Params = [TeamSpeak, TeamSpeakClient?] | [TeamSpeakClient];
+export default <T extends Params>(fn: (...args: T) => Promise<void>) =>
+  (...args: T) =>
     Promise.resolve(
       fn(...args).catch((error: Error) => catcher(error, fn.name))
     );
