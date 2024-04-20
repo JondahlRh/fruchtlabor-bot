c0ffc3bf353323bfee5c226338529879083fdeea
Jondahl Rhenius <jondahlrh@gmail.com>
2024-04-20 09:48:00 +0200

    added resuest param id error

--- src/classes/htmlErrors/index.ts
+++ src/classes/htmlErrors/index.ts
@@ -1,6 +1,13 @@
 import AuthError from "./AuthError";
 import IdError from "./IdError";
 import RequestBodyError from "./RequestBodyError";
+import RequestParamIdError from "./RequestParamIdError";
 import UnknownTeamSpeakError from "./UnknownTeamspeakError";
 
-export { AuthError, IdError, RequestBodyError, UnknownTeamSpeakError };
+export {
+  AuthError,
+  IdError,
+  RequestBodyError,
+  UnknownTeamSpeakError,
+  RequestParamIdError,
+};
