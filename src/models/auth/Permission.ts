78af16f898a28f5a4e4de03162eba2806312ac1f
Jondahl Rhenius <jondahlrh@gmail.com>
2024-05-30 20:03:30 +0200

    removed unused exports

--- src/models/auth/Permission.ts
+++ src/models/auth/Permission.ts
@@ -5,7 +5,7 @@ export const PermissionZodSchema = z.object({
   name: z.string(),
 });
 
-export type PermissionType = z.infer<typeof PermissionZodSchema>;
+type PermissionType = z.infer<typeof PermissionZodSchema>;
 
 const PermissionSchema = new mongoose.Schema<PermissionType>({
   name: { type: String, required: true },
