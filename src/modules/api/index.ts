70c248e5d355d42d02cca44bca32b28756682a2b
Jondahl Rhenius <jondahlrh@gmail.com>
2024-05-30 18:42:21 +0200

    removed admin routes

--- src/modules/api/index.ts
+++ src/modules/api/index.ts
@@ -4,7 +4,6 @@ import swaggerUi from "swagger-ui-express";
 import { TeamSpeak } from "ts3-nodejs-library";
 
 import { unknownRouteController, errorController } from "./controllers/error";
-import admin from "./routes/admin";
 import channel from "./routes/channel";
 import client from "./routes/client";
 import servergroup from "./routes/servergroup";
@@ -23,8 +22,6 @@ export default (teamspeak: TeamSpeak) => {
   app.use("/v1/servergroup", servergroup(teamspeak));
   app.use("/v1/channel", channel(teamspeak));
   app.use("/v1/client", client(teamspeak));
-
-  app.use("/v1/admin", admin());
 
   app.use(unknownRouteController);
   app.use(errorController);
