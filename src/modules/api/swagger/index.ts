4412eea5d55610106a8df6d0f13d980c747d00a5
Jondahl Rhenius <jondahlrh@gmail.com>
2024-07-02 19:36:37 +0200

    changed version number to 3.4

--- src/modules/api/swagger/index.ts
+++ src/modules/api/swagger/index.ts
@@ -9,7 +9,7 @@ const swaggerDocs = swaggerJSDoc({
         name: 'Jondahl "Gamix" Rhenius',
         email: "jondahlrh@gmail.com",
       },
-      version: "3.3.1",
+      version: "3.4",
     },
     components: {
       securitySchemes: {
